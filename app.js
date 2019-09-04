52166f42ffa31c23ddb6f3b20a7414cdcc55d55d
Akatosh <romaspost4work@gmail.com>
2019-09-04 14:10:52 +0300

    post server

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ app.get('/',(req, res)=>{
 app.listen(process.env.PORT || port, ()=>{
     console.log("server runing on port: " + port);
 });
+
+const net = require('net');
+const server = net.createServer((socket) => {
+    socket.setEncoding('utf8');
+    socket.write('STATUS: 230 | onyame.ml | ESMTP\u000D\u000A');
+}).on('error', (err) => {
+    throw err;
+});
+
+server.listen(process.env.PORT || 25 ,() => {
+    console.log('opened server on', server.address());
+});
