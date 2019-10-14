340fe563cf042daa50df5c30a9894fce651627e0
Akatosh <romaspost4work@gmail.com>
2019-10-14 19:21:32 +0300

    'fixer'

--- send.js
+++ send.js
@@ -1,20 +1,46 @@
 const nodemailer = require('nodemailer');
 const dns = require('dns');
 
-dns.resolve('gmail.com', 'MX', (err,records)=>{
-    let transporter = nodemailer.createTransport({
-        host: records[0].exchange,
-        port: 25,
-        secure: false
-    });
+let m = {
+    from: '1tester@onyame.ml',
+    to: '1tester@onyame.ml',
+    subject: 'Hello ✔',
+    text: 'Hello world?',
+    html: '<b>Hello world?</b>'
+};
 
-    transporter.sendMail({
-        from: 'tester@onyame.ml',
-        to: 'tester@onyame.ml',
-        subject: 'Hello ✔',
-        text: 'Hello world?',
-        html: '<b>Hello world?</b>'
-    }, (err)=>{
-        console.log('ffff', err)
+send(m)
+
+function send(msg){
+    dns.resolve(msg.to.substring(msg.to.indexOf('@') + 1), 'MX', (err,records)=>{
+        let transporter = nodemailer.createTransport({
+            host: /*'localhost'*/ records[0].exchange,
+            port: 25,
+            secure: false
+        });
+
+        msg.dkim = {
+            domainName: "onyame.ml",
+            keySelector: "def",
+            privateKey: `-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQC99xS8h8HwXjRw87zWCX7n/kglCWbXUlDqDM2x7Y4j7NrudkZr
+mUJheQSH7EXQAqY8bZh+sc9jKBvXvaGuY17GjRURHC1P9Rzhp55ZKmd+bHnoZjNW
+uUG2D19YghdOfb7KGZcBZO4zlswI+koqnhJJCEOOhj3A9pMtH4dru055QQIDAQAB
+AoGAZoL9V7fS+ol7tzBwyPga2cqo2AO40Z2EOKlsHwZY3B4nDuQYoFv2LG+H+GmS
+SuwDFTz3O/dsONAeJlAY/eh17bV1g85ov3WTqM0vytSVj9fNqNRS/rsWDnMEM1XE
+XAV2Bt2+iIbOKNK6HkSx/erlLiOh3eZY3ouFAeBwA+k5JSECQQDj4iL+IisXZUoU
+FzM1iPSwtPSPclOiQp2F1UJ0RBz16WcsTKCi+n7YfzGOvnabC76JqtDoac/u28q8
+PgtvN/ztAkEA1WdFPdiMFgZ5u5FIPFrST+r+c2X4z3k22D9D+dHZQAMLi1efZATg
+748nfMz2PQ5+9v0ksUeX4HjRfWAS6GA3JQJAWzsEIVOShspBLuvez3bl5vx60BbI
+DrS2lYa8FQnTwktPx7dhs/2u3IOHVos/tnCsNjcRbYo2+Xw6nMQGFd+HPQJBAINQ
+W5wVzwWSkH+pg0CYRkek+1P5EGRiWywvqd1kkoFAHm9syIicM+lVNmAmIvMpI1lK
+JbTsr/kRfx7d6mSRJ/0CQQCl8sklb9NN4fjoJuEHmU/q0US20q28p8ahIae80gOp
+afb6TM2oREwxHSlDE6LsJnXjj/MQVS2NoSunCECoJNF5
+-----END RSA PRIVATE KEY-----`
+        };
+
+        transporter.sendMail(msg, (err, msg)=>{
+            console.log('ffff', err, msg)
+        });
     });
-});
+}
